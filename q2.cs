d65114bd776276c422d6b601d6fcf9a536ae3f56
khimsuriya <kkhimsuriya805@rku.ac.in>
2025-07-15 15:09:17 +0530

    q2

---
+++ q2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_1
+{
+    internal class q2
+    {
+        static void Main(String[] arg)
+        {
+            Console.WriteLine("Name: MAKAWANA YASHVI");
+            Console.WriteLine("DOB: 24/02/2004");
+            Console.WriteLine("Address: Mavdi Chokdi");
+            Console.WriteLine("City: Rajkot");
+            Console.WriteLine("Pincode: 360003");
+            Console.WriteLine("state: Gujrat");
+            Console.WriteLine("Country: India");
+            Console.WriteLine("Email: ymakwana259@rku.ac.in");
+
+        }
+    }
+}
