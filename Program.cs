19d5ef623228c24a8ca65467f7745f1f7d9458af
khimsuriya <kkhimsuriya805@rku.ac.in>
2025-07-15 15:06:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("24SOECE13033 - MAKAWANA YASHVI");
+            Console.WriteLine("Hello World");
+        }
+    }
+}
